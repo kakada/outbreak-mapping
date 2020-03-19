8e9d9a8b84eefa1957d40c058a5a11e7fcc8f56e
Borey Lim <lim.borey@gmail.com>
2020-03-19 23:59:56 +0700

    set km as default locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_action :set_locale
+
   layout :layout_by_resource
 
   private
     def layout_by_resource
       devise_controller? ? "minimal" : "application"
+    end
+
+    def set_locale
+      I18n.locale = params[:locale] || :km
     end
 end
