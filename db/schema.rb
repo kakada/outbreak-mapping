1c0cca1923d802c0406095b3bd7fd78912be5f2b
radinreth <radin.reth@gmail.com>
2020-03-20 20:55:18 +0700

    Drop old tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_143558) do
+ActiveRecord::Schema.define(version: 2020_03_20_135245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,32 +26,6 @@ ActiveRecord::Schema.define(version: 2020_03_18_143558) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "nations", force: :cascade do |t|
-    t.string "name", default: ""
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_nations_on_name"
-  end
-
-  create_table "reports", force: :cascade do |t|
-    t.integer "cases", default: 0
-    t.integer "heals", default: 0
-    t.integer "deaths", default: 0
-    t.bigint "site_id", null: false
-    t.bigint "nation_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["nation_id"], name: "index_reports_on_nation_id"
-    t.index ["site_id"], name: "index_reports_on_site_id"
-  end
-
-  create_table "sites", force: :cascade do |t|
-    t.string "code", default: ""
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["code"], name: "index_sites_on_code"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -64,6 +38,4 @@ ActiveRecord::Schema.define(version: 2020_03_18_143558) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reports", "nations"
-  add_foreign_key "reports", "sites"
 end
