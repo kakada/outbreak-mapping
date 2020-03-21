5f0049e5f86555f2c5b54cbf2965e2952aa2a2b2
radinreth <radin.reth@gmail.com>
2020-03-21 15:40:19 +0700

    Merge into one report file

--- app/models/report_detail.rb
+++ app/models/report_detail.rb
@@ -1,17 +1,6 @@
 class ReportDetail < ApplicationRecord
-  attr_accessor :location_code
   belongs_to :report
 
   validates :field_name, :field_value, presence: true
   validates :display_order, numericality: { greater_than: 0 }
-
-  before_validation :ensure_reports
-
-  def ensure_reports
-    begin
-      self.report = Report.find_by(location_code: location_code)
-    rescue Exception => e
-      logger.log("#{location_code}: #{e.inspect}")
-    end
-  end
 end
--- app/services/report_service.rb
+++ app/services/report_service.rb
@@ -2,15 +2,21 @@ require "csv"
 
 class ReportService
   def self.import(path, original_filename)
-    file_name = File.basename(original_filename, ".csv")
-    klass = file_name.classify.constantize
-
-    Report.destroy_all if file_name.inquiry.reports?
+    Report.destroy_all
 
     ::CSV.foreach(path, headers: true, encoding: "bom|utf-8") do |row|
       hash = row.to_hash
 
-      klass.create(hash)
+      report_params = {}
+
+      report_params[:location_code] = hash.delete("location_code")
+      report_params[:total_cases] = hash.delete("total_cases")
+      report_params[:new_cases] = hash.delete("new_cases")
+      report_params[:recovered_cases] = hash.delete("recovered_cases")
+      report_params[:death_cases] = hash.delete("death_cases")
+
+      report = Report.create(report_params)
+      report.report_details.create(hash)
     end
   end
 end
