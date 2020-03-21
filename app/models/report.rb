4b516e5845cc36043d2acb8f5909c60f08325709
kakada <kakada.chheang@gmail.com>
2020-03-21 18:47:26 +0700

    Refactor report service model

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,4 +5,6 @@ class Report < ApplicationRecord
   validates :location_code, uniqueness: true
   validates :total_cases, numericality: { greater_than_or_equal_to: 0 }
   validates :new_cases, numericality: { greater_than_or_equal_to: 0 }
+
+  belongs_to :location, class_name: 'Location', foreign_key: 'location_code'
 end
--- app/services/report_service.rb
+++ app/services/report_service.rb
@@ -2,21 +2,47 @@ require "csv"
 
 class ReportService
   def self.import(path, original_filename)
-    Report.destroy_all
+    Report.transaction do
+      Report.destroy_all
 
-    ::CSV.foreach(path, headers: true, encoding: "bom|utf-8") do |row|
-      hash = row.to_hash
-
-      report_params = {}
-
-      report_params[:location_code] = hash.delete("location_code")
-      report_params[:total_cases] = hash.delete("total_cases")
-      report_params[:new_cases] = hash.delete("new_cases")
-      report_params[:recovered_cases] = hash.delete("recovered_cases")
-      report_params[:death_cases] = hash.delete("death_cases")
-
-      report = Report.create(report_params)
-      report.report_details.create(hash)
+      ::CSV.foreach(path, headers: true, encoding: "bom|utf-8") do |row|
+        import_row row
+      end
     end
+  end
+
+  protected
+
+  def self.import_row row
+    whitelist_hash = whitelist_columns row.to_hash
+
+    report = Report.new(protected_report_params(whitelist_hash))
+    report.save do
+      whitelist_hash.each do |k, v|
+        next if Report.attribute_names.include?(k)
+
+        report.report_details.create(field_name: k, field_value: v)
+      end
+    end
+  end
+
+  def self.whitelist_columns hash
+    unused_columns = ENV.fetch('UNUSED_CSV_COLUMNS') { [] }
+
+    unused_columns.split(',').each do |column_name|
+      hash.delete(column_name)
+    end
+
+    hash
+  end
+
+  def self.protected_report_params hash
+    params = {}
+    
+    Report.attribute_names.each do |attribute_name|
+      params[attribute_name] = hash.delete(attribute_name)
+    end
+
+    params
   end
 end
