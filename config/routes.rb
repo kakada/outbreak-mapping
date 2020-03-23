e1c7350fc412d861fd2f2129eb1cf1f6f29941b0
radinreth <radin.reth@gmail.com>
2020-03-23 16:36:22 +0700

    Remove :registrations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root to: "home#index"
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :reports, only: [:index, :new, :create]
   resources :videos, only: [:index, :new, :create]
