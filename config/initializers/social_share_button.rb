76ce12877d3af2e0575a5382e190049e3eb170c0
Sokly <hengsokly23@gmail.com>
2020-03-23 14:20:18 +0700

    add social share in mobile

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -2,5 +2,5 @@ SocialShareButton.configure do |config|
   # config.allow_sites = %w(twitter facebook google_plus weibo qq douban google_bookmark
   #                         delicious tumblr pinterest email linkedin wechat vkontakte
   #                         xing reddit hacker_news telegram odnoklassniki whatsapp_app whatsapp_web)
-  config.allow_sites = %w(facebook twitter)
+  config.allow_sites = %w(facebook twitter linkedin telegram email wechat whatsapp_web)
 end
