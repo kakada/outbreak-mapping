1969ba4746a3ff72628cf9b1c6dec2e709117e03
kakada <kakada.chheang@gmail.com>
2020-03-28 09:41:40 +0700

    Sort report case listing (Fixes #57)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@
 class HomeController < ApplicationController
   def index
     @locations = Location.all
-    @reports = Report.includes(:location, :report_details).all
+    @reports = Report.includes(:location, :report_details).order('total_cases desc, updated_at desc').all
     @json_reports = @reports.to_json(include: { location: { only: [:name_km, :latitude, :longitude] } })
     @summary_report = summary_report
     @device = DeviceDetector.new(request.user_agent)
