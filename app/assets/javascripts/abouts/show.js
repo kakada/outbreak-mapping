7b70a6f65cbc06ed0a345472447e6c8918c9ee93
Borey Lim <lim.borey@gmail.com>
2020-03-31 10:49:42 +0700

    fix scroll back menu on mobile

--- app/assets/javascripts/abouts/show.js
+++ app/assets/javascripts/abouts/show.js
@@ -4,39 +4,6 @@ OM.AboutsShow = (() => {
   }
 
   function init() {
-    initMobile();
+    OM.Common.MobileMenu.onScrollContainer();
   }
-
-  function initMobile() {
-    if ($(".mobile").length > 0) {
-      onScrollContainer();
-    }
-  }
-
-  function onScrollContainer() {
-    const scrollUp = "scroll-up";
-    const scrollDown = "scroll-down";
-    const body = document.body;
-    let lastScroll = 0;
-
-    $(document).on('scroll', function() {
-      const currentScroll = window.pageYOffset;
-      if (currentScroll == 0) {
-        body.classList.remove(scrollUp);
-        return;
-      }
-
-      if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-        // down
-        body.classList.remove(scrollUp);
-        body.classList.add(scrollDown);
-      } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
-        // up
-        body.classList.remove(scrollDown);
-        body.classList.add(scrollUp);
-      }
-      lastScroll = currentScroll;
-    });
-  }
-
 })();
--- app/assets/javascripts/developer_guides/index.js
+++ app/assets/javascripts/developer_guides/index.js
@@ -4,6 +4,12 @@ OM.Developer_guidesIndex = (() => {
   }
 
   function init() {
+    OM.Common.MobileMenu.onScrollContainer();
+    initCodeHighlight();
+  }
+
+  function initCodeHighlight() {
+    hljs.initHighlightingOnLoad();
     hljs.initHighlighting.called = false;
     hljs.initHighlighting();
     new ClipboardJS('.btn');
@@ -12,20 +18,6 @@ OM.Developer_guidesIndex = (() => {
       trigger: 'click',
       placement: 'bottom'
     });
-
-    function setTooltip(btn, message) {
-      $(btn).tooltip('hide')
-        .attr('data-original-title', message)
-        .tooltip('show');
-    }
-
-    function hideTooltip(btn) {
-      setTimeout(function() {
-        $(btn).tooltip('hide');
-      }, 1000);
-    }
-
-    // Clipboard
 
     var clipboard = new ClipboardJS('button');
 
@@ -40,21 +32,15 @@ OM.Developer_guidesIndex = (() => {
     });
   }
 
-  function initCodeHighlight() {
-    hljs.initHighlightingOnLoad();
-    // var pres = document.querySelectorAll("pre>code");
-    // for (var i = 0; i < pres.length; i++) {
-    //     hljs.highlightBlock(pres[i]);
-    // }
+  function setTooltip(btn, message) {
+    $(btn).tooltip('hide')
+      .attr('data-original-title', message)
+      .tooltip('show');
+  }
 
-    // var options = {   // optional
-    //     contentSelector: "#ArticleBody",
-
-    //     // CSS class(es) used to render the copy icon.
-    //     copyIconClass: "fas fa-copy",
-    //     // CSS class(es) used to render the done icon.
-    //     checkIconClass: "fas fa-check text-success"
-    // };
-    // window.highlightJsBadge(options);
+  function hideTooltip(btn) {
+    setTimeout(function() {
+      $(btn).tooltip('hide');
+    }, 1000);
   }
 })();
