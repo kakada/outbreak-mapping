4b43e0577ce3fa73181fd268e43af8dd4e330a1a
Borey Lim <lim.borey@gmail.com>
2020-03-31 18:15:28 +0700

    implement create/edit setting

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,5 +2,41 @@
 
 class SettingsController < AdminController
   def show
+    @setting = Setting.first
   end
+
+  def new
+    @setting = Setting.new
+  end
+
+  def create
+    @setting = Setting.new(setting_params)
+
+    if @setting.save
+      redirect_to setting_path
+    else
+      flash.now[:alert] = @sop.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @setting = Setting.first
+  end
+
+  def update
+    @setting = Setting.first
+
+    if @setting.update(setting_params)
+      redirect_to setting_path
+    else
+      flash.now[:alert] = @sop.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+    def setting_params
+      params.require(:setting).permit(:last_updated)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
 
   resources :developer_guides, only: [:index]
 
-  resource :setting, only: [:show]
+  resource :setting, except: [:destroy]
 end
