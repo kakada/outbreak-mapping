1d5f2544ee9ecde7e97c0a8250b209e370876e02
kakada <kakada.chheang@gmail.com>
2021-05-02 11:53:07 +0700

    Display numberic in a pretty format, thousand comma separate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
     @json_reports = @reports.to_json(include: { location: { only: [:name_km, :latitude, :longitude] }, report_details: { only: [:field_name, :field_value] } })
     @summary_report = summary_report
     @json_summary_report =summary_report.to_json(include: { location: { only: [:name_km, :latitude, :longitude] } })
+    @has_detail_info = Report.has_detail?
     @videos = Video.order(:display_order)
     @setting = Setting.first || Setting.new
   end
