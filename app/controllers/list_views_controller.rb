c2ad8c03ca5d18a1c2a66390e7c0897f78cb1397
kakada <kakada.chheang@gmail.com>
2021-07-08 16:30:22 +0700

    WIP

--- app/controllers/list_views_controller.rb
+++ app/controllers/list_views_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-class ListViewsController < ApplicationController
+class ListViewsController < HomeController
   def index
-    @reports = Report.includes(:location, :report_details).order('total_case desc, updated_at desc').all
-    @json_reports = @reports.to_json(include: { location: { only: [:name_km] } })
-    @summary_report = summary_report
-    @setting = Setting.first || Setting.new
+    super
+    # @reports = Report.includes(:location, :report_details).order('total_case desc, updated_at desc').all
+    # @json_reports = @reports.to_json(include: { location: { only: [:name_km] } })
+    # @summary_report = summary_report
+    # @setting = Setting.first || Setting.new
   end
 end
